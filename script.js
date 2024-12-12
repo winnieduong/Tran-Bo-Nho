027a72c63e02e89f2ad61a6cb3a7f400c25e5d81
winnieduong <khanhuyenhduong@gmail.com>
2024-12-11 22:11:59 -0500

    change footer

--- script.js
+++ script.js
@@ -33,10 +33,3 @@ function minimizeNotification(button) {
   const notification = button.closest('.notification');
   notification.style.display = 'none'; 
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  var encodedText = '&#68;&#101;&#115;&#105;&#103;&#110;&#32;&#98;&#121;&#32;&#80;&#97;&#110;&#98;&#97;&#112;';
-  var footer = document.createElement("a");
-  footer.innerHTML = encodedText; 
-  document.body.appendChild(footer); 
-});
